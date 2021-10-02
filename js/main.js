1835a2609cab4e538520c2a5cae2356a77ae90ba
Margarita Puzanova <margarett.puzanova@gmail.com>
2021-10-02 15:01:15 +0300

    Вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,22 +1,42 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+// Функция, возвращающая случайное целое число из
+// переданного диапазона включительно.
 
-function randomInteger(min, max) {
+function getRandomInteger(min, max) {
   // случайное число от min до (max+1)
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+  if (min >= max) {
+    return null;
+  }
+  if (min < 0) {
+    return null;
+  }
+  if (max < 0) {
+    return null;
+  }
+
+  const randomInteger = min + Math.random() * (max + 1 - min);
+  return Math.floor(randomInteger);
 }
 
-randomInteger(1,3);
+getRandomInteger(1, 3);
 
 // Источник: https://learn.javascript.ru/task/random-int-min-max
 
 
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+//Функция, возвращающая случайное число с плавающей
+// точкой из переданного диапазона включительно.
 
-function random(min, max) {
-  return min + Math.random() * (max - min);
+function getRandomFloat(min, max, decimal) {
+  if (min < 0 || max < 0) {
+    return null;
+  }
+  if (max <= min) {
+    return null;
+  }
+  const randomFloat = min + Math.random() * (max - min);
+  const fixedRandomFloat = Number(randomFloat.toFixed(decimal));
+  return fixedRandomFloat;
 }
 
-random(1,4);
+getRandomFloat(1, 4, 2);
 
 // Источник:https://learn.javascript.ru/number
