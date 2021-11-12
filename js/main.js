19a57703eb9ce13dd6d4cfcdfdb912391d458f6f
Margarita Puzanova <margarett.puzanova@gmail.com>
2021-11-12 17:09:52 +0300

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -6,8 +6,6 @@ import { createNewAd } from '.create-ads';
 const adsArray = [];
 
 for (let i = 0; i < 10; i++) {
-
   const element = createNewAd(i);
-
   adsArray[i] = element;
 }
---
+++ js/markup-generation.js
@@ -0,0 +1,9 @@
+//Функция для отрисовки 1го объявления
+
+function createCard (ad) {
+   // получение ДОМ элемента карточки
+}
+
+const card = document.querySelector('#card');
+const clonedCard = card.cloneNode(true);
+console.log(clonedCard);
